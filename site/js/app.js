74902aae7253dc901f7460f6c6d7c5c27b6763b9
Nutakoe <46197580+Replayer1@users.noreply.github.com>
2021-05-23 14:02:26 +0300

    Add files via upload

--- site/js/app.js
+++ site/js/app.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
       $('body').toggleClass('lock');
    });
 
+   
    $('.star').click(function(event) {
       $(this).toggleClass('bi-star-fill');
       $(this).toggleClass('bi-star');
    });
+});
 
-
-
+$(document).ready(function() {
+   
    function loadMore(button, element) {
       $(`${button}`).on('click', function (e) {
       e.preventDefault()
@@ -21,27 +23,48 @@ $(document).ready(function() {
       })
    }
 
-   loadMore('#loadMore', '.produckt__box:hidden')
+   loadMore('#loadMore', '.load__more-item-1:hidden')
    loadMore('#loadMore-2', '.load__more-item-2:hidden')
+   loadMore('#loadMore-3', '.load__more-item-3:hidden')
 
 });
 
 
+$(document).ready(function() {
+   
+   function loadMoreTwo(button, element) {
+      $(`${button}`).on('click', function (e) {
+      e.preventDefault()
+      $(`${element}`).slice(0, 2).slideDown().addClass('opacity')
+      if ($(`${element}`).length == 0) {
+         $(`${button}`).text('No Content').addClass('noContent')
+      }
+      })
+   }
 
-   // $(".produckt__box").slice(0, 3).show();
-   // $("#loadMore").on("click", function(e){
-   //    e.preventDefault();
-   //    $(".produckt__box:hidden").slice(0, 3).slideDown().addClass('opacity');
-   //    if($(".produckt__box:hidden").length == 0) {
-   //       $("#loadMore").text("No Content").addClass("noContent");
-   //    }
-   // });
+   loadMoreTwo('#loadMore-4', '.load__more-item-4:hidden')
+   
+});
 
-   // $(".load__more-item-2").slice(0, 3).show();
-   // $("#loadMore-2").on("click", function(e){
-   //    e.preventDefault();
-   //    $(".load__more-item-2:hidden").slice(0, 3).slideDown();
-   //    if($(".load__more-item-2:hidden").length == 0) {
-   //       $("#loadMore-2").text("No Content").addClass("noContent");
-   //    }
-   // });
+
+
+
+// $(".produckt__box").slice(0, 3).show();
+// $("#loadMore").on("click", function(e){
+//    e.preventDefault();
+//    $(".produckt__box:hidden").slice(0, 3).slideDown().addClass('opacity');
+//    if($(".produckt__box:hidden").length == 0) {
+//       $("#loadMore").text("No Content").addClass("noContent");
+//    }
+// });
+// });
+
+// $(document).ready(function() {
+// $(".load__more-item-2").slice(0, 3).show();
+// $("#loadMore-2").on("click", function(e){
+//    e.preventDefault();
+//    $(".load__more-item-2:hidden").slice(0, 3).slideDown().addClass('opacity');
+//    if($(".load__more-item-2:hidden").length == 0) {
+//       $("#loadMore-2").text("No Content").addClass("noContent");
+//    }
+// });
